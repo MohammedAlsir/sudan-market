fafaf5d6abbc8f35a6298eb8f1b9cf4c135443a4
MohammedAlsir <m7mdb3sher@gmail.com>
2021-03-26 14:05:22 +0200

    add laravel files

---
+++ database/migrations/2021_03_23_121355_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('visibility');
+            $table->string('price');
+            $table->string('new_price');
+            $table->string('img');
+            $table->string('discount');
+            $table->foreignId('company_id')->nullable()->constrained();
+            $table->foreignId('section_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2021_03_23_121635_create_advertisings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertisingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertisings', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('img');
+            $table->string('state');
+            $table->foreignId('company_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisings');
+    }
+}
